882044cc892a68315b266c3cd02636bd7397ac2e
bagaking <zfaustk@qq.com>
2019-04-09 14:42:00 +0800

    feat (app): default local config path

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,14 +6,14 @@ import * as Path from 'path';
 import * as fs from "fs-extra";
 
 async function main() {
-    commander.version('0.0.1')
+    commander.version('0.1.0')
         .command('start')
         .description('start running login server')
         .option('-d, --development',
-            '(default env setting) similar to set NODE_ENV=development, and will read conf.development.json at executing position as config by default',
+            '(default env setting) similar to set NODE_ENV=development, and will read login.development.json at executing position as config by default',
             () => process.env.NODE_ENV = 'development')
         .option('-p, --production',
-            'similar to set NODE_ENV=production, and will read conf.production.json at executing position as config by default',
+            'similar to set NODE_ENV=production, and will read login.production.json at executing position as config by default',
             () => process.env.NODE_ENV = 'production')
         .option('-c, --config <path>',
             'set config path, and the specified conf will override the default one set by NODE_ENV',
@@ -21,7 +21,13 @@ async function main() {
         .option('-P, --port <port>',
             'the port to serve api, will override the setting in config file, 11801 by default')
         .action((options) => {
-            Global.setConf(Path.resolve(__dirname, `../conf.default.json`), false);
+            try {
+                Global.setConf(Path.resolve(process.cwd(), `./login.${process.env.NODE_ENV || "development"}.json`), false);
+            }catch (e) {
+                Global.setConf(Path.resolve(__dirname, `../conf.default.json`), false);
+            }
+
+            console.log("config path :", Global.confPath);
             Global.conf.port = (options && options.port) || Global.conf.port || 11801;
             const api = new ApiApplication();
             api.start(Global.conf.port);
@@ -31,7 +37,7 @@ async function main() {
         .description('extract default config to a file')
         .option('-p, --path <path>', 'the export path')
         .action((options) => {
-            let extractPath = (options && options.path) || './conf.default.json';
+            let extractPath = (options && options.path) || './login.development.json';
             extractPath = Path.isAbsolute(extractPath) ? extractPath : Path.resolve(process.cwd(), extractPath);
             fs.copyFileSync(Path.resolve(__dirname, `../conf.default.json`), extractPath);
             process.exit(1);
@@ -41,9 +47,9 @@ async function main() {
 }
 
 main().then(() => {
-    console.info('app start running.');
+    console.info('running @khgame/loginsvr succeeded.');
 }).catch((reason => {
-    console.error(reason + '\nrunning app failed.');
+    console.error(reason + '\nrunning @khgame/loginsvr failed.');
     process.exit(1);
 }));
 
