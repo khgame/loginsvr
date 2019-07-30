e335d690bb932e97874b0468e082ec2a19601881
bagaking <zfaustk@qq.com>
2019-07-30 22:57:04 +0800

    feat: update mail content

--- src/logic/loginService.ts
+++ src/logic/loginService.ts
@@ -86,13 +86,13 @@ export class LoginService {
         const html = `
 <div style="width: 800px;background-color: black;padding: 50px;margin: 0;">
   <p style="margin: 0;">
-    <strong style="color:white;font-size: 2rem;">你好</strong>
+    <strong style="color:white;font-size: 2rem;">你好！</strong>
   </p>
   <div style="color:white;font-size:1rem;padding: 2rem;background-color: #e48600;width:400px;margin-top: 30px;" >
-    <p style="margin: 0;">激活链接标题</p>
-    <a href="http://${url}" style="display: block; color:white;margin-top: 1rem;">激活链接: ${redisKey}</a>
+    <p style="margin: 0;">请点击以下链接完成激活：</p>
+    <a href="http://${url}" style="display: block; color:white;margin-top: 1rem;">激活链接：${redisKey}</a>
   </div>
-  <p style="color:white;margin: 0;margin-top: 2rem;">邮件内容描述</p>
+  <!--<p style="color:white;margin: 0;margin-top: 2rem;">邮件内容描述</p>-->
 </div>
 `;
 
@@ -177,13 +177,13 @@ export class LoginService {
         const html = `
 <div style="width: 800px;background-color: black;padding: 50px;margin: 0;">
   <p style="margin: 0;">
-    <strong style="color:white;font-size: 2rem;">你好</strong>
+    <strong style="color:white;font-size: 2rem;">你好！</strong>
   </p>
   <div style="color:white;font-size:1rem;padding: 2rem;background-color: #e48600;width:400px;margin-top: 30px;" >
-    <p style="margin: 0;">找回密码</p>
-    <a href="http://${url}" style="display: block; color:white;margin-top: 1rem;">激活链接: ${redisKey}</a>
+    <p style="margin: 0;">请点击以下链接重设密码：</p>
+    <a href="http://${url}" style="display: block; color:white;margin-top: 1rem;">重设密码链接：${redisKey}</a>
   </div>
-  <p style="color:white;margin: 0;margin-top: 2rem;">邮件内容描述</p>
+  <!--<p style="color:white;margin: 0;margin-top: 2rem;">邮件内容描述</p>-->
 </div>
 `;
 
