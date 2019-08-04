aea6c113f9ba8e0b20ba91185037721f17ca72cf
bagaking <zfaustk@qq.com>
2019-08-04 15:01:40 +0800

    feat (login): add template reading util

---
+++ src/logic/util/file.ts
@@ -0,0 +1,51 @@
+import {genMemCache, turtle} from "@khgame/turtle";
+import * as Path from "path";
+import * as fs from "fs-extra";
+
+const superFlags = [
+    {
+        from: /{{ip_public}}/,
+        to: () => turtle.runtime.ip_public
+    },
+    {
+        from: /{{ip_locale}}/,
+        to: () => turtle.runtime.ip
+    }
+];
+
+const cache = genMemCache();
+
+export function readTemplate(
+    relativePath: string,
+    replace: Array<{ from: string | RegExp, to: string | (() => string) }>
+): string | null {
+
+    let ret: string | null = cache.get(relativePath) || null;
+    if (!ret) {
+        const filePth = Path.resolve(turtle.runtime.cwd, relativePath);
+        const exist = fs.existsSync(filePth);
+        if (!exist) {
+            return null;
+        }
+        ret = fs.readFileSync(filePth, {encoding: "UTF-8"});
+    }
+
+    if (!ret) { return null; }
+
+    for (let iRep in replace) {
+        const replacer = replace[iRep];
+        ret = ret.replace(
+            replacer.from,
+            typeof replacer.to === "string" ? replacer.to : replacer.to()
+        );
+    }
+
+    for (let iRep in superFlags) {
+        const replacer = replace[iRep];
+        ret = ret.replace(
+            replacer.from,
+            typeof replacer.to === "string" ? replacer.to : replacer.to()
+        );
+    }
+    return ret;
+}
